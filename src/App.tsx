72ae84e835455aa21eca3a8f03a82c68b14f9c9b
kiokoeric <kiokoerick040@gmail.com>
2024-06-04 20:48:37 +0300

    Creation of the Popular Page in AniWorld

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Anime from './Pages/Anime/Anime';
 import Genre from './Pages/Genre/Genre';
 import Manga from './Pages/Manga/Manga';
 import Create from './Pages/Create/Create';
+import Popular from './Pages/Popular/Popular';
 
 function App() {
 
@@ -15,6 +16,7 @@ function App() {
         <Route path='/Home' element={ <Anime />  } />
         <Route path='/Genre' element={ <Genre /> } />
         <Route path='/Manga' element={ <Manga /> } />
+        <Route path='/Popular' element={ <Popular /> } />
         <Route path='/Create' element={ <Create /> } />
       </Routes>
     </div>
